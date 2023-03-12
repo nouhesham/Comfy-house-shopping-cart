a5b3af41bd34c2d6d1839b6190a8fe64f48bf3b7
Nourhan <nourhanhesham263@gmail.com>
2023-03-12 14:24:39 +0200

    adding classes variables

--- comfy.js
+++ comfy.js
@@ -1,1 +1,18 @@
 "strict";
+const cartbutton = document.querySelector(".cart-btn");
+const closeCartbtn = document.querySelector(".close-cart");
+const clearCartbtn = document.querySelector(".clear-cart");
+const CartDom = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDom = document.querySelector(".products-center");
+const showcart = document.querySelector(".showcart");
+//cart items
+let cart = [];
+
+cartbutton.addEventListener("click", function () {
+  cartOverlay.classList.add("showcart");
+  cartOverlay.classList.add("transparentBcg");
+});
