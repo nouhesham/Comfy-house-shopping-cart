920be29227e8058d0ae6d9f0029d9ad4896ac503
Nourhan <nourhanhesham263@gmail.com>
2023-03-13 11:48:39 +0200

    adding more variables

--- comfy.js
+++ comfy.js
@@ -9,11 +9,35 @@ const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
 const productsDom = document.querySelector(".products-center");
 const showcart = document.querySelector(".showcart");
+const iconclose = document.querySelector("#icon");
+
 //cart items
 let cart = [];
 
-cartbutton.addEventListener("click", function () {
+const show = function () {
   cartOverlay.classList.add("showcart");
   cartOverlay.classList.add("transparentBcg");
   CartDom.style.transform = "translateX(0)";
+};
+const close = function () {
+  cartOverlay.classList.remove("showcart");
+  cartOverlay.classList.remove("transparentBcg");
+  CartDom.style.transform = "translateX(100%)";
+  console.log("hello");
+};
+
+cartbutton.addEventListener("click", show);
+iconclose.addEventListener("click", function () {
+  console.log("hello");
 });
+
+document.addEventListener("keydown", function (e) {
+  if (
+    e.key === "Escape" &&
+    !cartOverlay.classList.contains("hidden") &&
+    !CartDom.classList.contains("translateX(100%)")
+  ) {
+    this.close();
+  }
+});
+console.log(iconclose);
